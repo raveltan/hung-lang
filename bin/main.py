d9dd5c1370bce4862bfabab6b43436f1d5d56ad1
raveltan <raveltanjaya@outlook.com>
2020-01-05 18:07:28 +0700

    reformated the whole python code

--- bin/main.py
+++ bin/main.py
@@ -1,26 +1,34 @@
-import sys,os
-try: 
+import sys, os
+
+try:
     import termcolor
-except :
-    os.system('cls' if os.name == 'nt' else 'clear')
-    print('''    __  __            ______
+except:
+    os.system("cls" if os.name == "nt" else "clear")
+    print(
+        """    __  __            ______
    / / / /_  ______  / ____/
   / /_/ / / / / __ \/ / __  
  / __  / /_/ / / / / /_/ /  
 /_/ /_/\__,_/_/ /_/\____/   
-''')
-    print('HunG 0.0.4 [Alpha,29-12-2019|11.40AM]')
-    print('Depedency not satisfied, termcolor not found!\n\n')
-    print('Should we attempt to perform installation of [Termcolor]?')
-    data = input('Y/N >')
+"""
+    )
+    print("HunG 0.0.4 [Alpha,29-12-2019|11.40AM]")
+    print("Depedency not satisfied, termcolor not found!\n\n")
+    print("Should we attempt to perform installation of [Termcolor]?")
+    data = input("Y/N >")
     if data.lower() == "y":
-        streams = os.popen('py -m pip install termcolor' if os.name == 'nt' else 'python3 -m pip install termcolor')
+        streams = os.popen(
+            "py -m pip install termcolor"
+            if os.name == "nt"
+            else "python3 -m pip install termcolor"
+        )
         stream = streams.readlines()
-        result = stream[len(stream)-1]
-        print('\n'+str(result))
-        if len(result.split('Successfully installed')) > 1:
-            print('Please re-run the program!\n')
-            input('Press any key to continue...')
+        result = stream[len(stream) - 1]
+        print("\n" + str(result))
+        if len(result.split("Successfully installed")) > 1:
+            print("Please re-run the program!\n")
+            input("Press any key to continue...")
 else:
     import shell
+
     shell.main()
--- bin/shell.py
+++ bin/shell.py
@@ -1,42 +1,56 @@
-import lexer,hung_parser,interpreter
-import sys,os
+import lexer, hung_parser, interpreter
+import sys, os
 import termcolor
 from time import sleep
 
-#helper functions
+# helper functions
 def write_delay(text):
     for char in text:
         sleep(0.005)
         sys.stdout.write(char)
         sys.stdout.flush()
-def es(name,desc):
-        SPACING = 20 - len(name)
-        print(name + ' '*SPACING+'| ' +desc)
+
+
+def es(name, desc):
+    SPACING = 20 - len(name)
+    print(name + " " * SPACING + "| " + desc)
+
 
 def print_help():
-    es('.debug','Show a verbose log of the interpreter.')
-    es('.vardump','Dump the variable symbol table to the terminal.')
-    es('.clear','Clear the terminal window.')
-    es('.exit','Exit the HunG interpreter.')
-    es('.version','Show the version of the HunG interpreter.')
-    es('.started','Show the getting started guide of HunG.')
-    es('.help','Show this dialog')
+    es(".debug", "Show a verbose log of the interpreter.")
+    es(".vardump", "Dump the variable symbol table to the terminal.")
+    es(".clear", "Clear the terminal window.")
+    es(".exit", "Exit the HunG interpreter.")
+    es(".version", "Show the version of the HunG interpreter.")
+    es(".started", "Show the getting started guide of HunG.")
+    es(".help", "Show this dialog")
+
 
 def title():
-    print(termcolor.colored('''    __  __            ______
+    print(
+        termcolor.colored(
+            """    __  __            ______
    / / / /_  ______  / ____/
   / /_/ / / / / __ \/ / __  
  / __  / /_/ / / / / /_/ /  
 /_/ /_/\__,_/_/ /_/\____/   
-''',color='cyan'))
-    print(termcolor.colored('HunG 0.0.4 [Alpha,29-12-2019|11.40AM]',color='cyan'))
-    print(f'Running on top of Python {sys.version}\nUse .help to view help page\n')
+""",
+            color="cyan",
+        )
+    )
+    print(termcolor.colored("HunG 0.0.4 [Alpha,29-12-2019|11.40AM]", color="cyan"))
+    print(f"Running on top of Python {sys.version}\nUse .help to view help page\n")
+
 
 def clear_screen():
-    os.system('cls' if os.name == 'nt' else 'clear')
+    os.system("cls" if os.name == "nt" else "clear")
+
+
 def getting_started():
     clear_screen()
-    print(termcolor.colored('''
+    print(
+        termcolor.colored(
+            """
      (  (           (                        
  )\))(   '   (  )\             )      (  
 ((_)()\ )   ))\((_) (   (     (      ))\ 
@@ -44,13 +58,20 @@ _(())\_)() /((_)_   )\  )\    )\  ' /((_)
 \ \((_)/ /(_)) | | ((_)((_) _((_)) (_))  
  \ \/\/ / / -_)| |/ _|/ _ \| '  \()/ -_) 
   \_/\_/  \___||_|\__|\___/|_|_|_| \___| 
-    ''',color='yellow'))
-    write_delay('Welcome to getting started with HunG\n\n')
+    """,
+            color="yellow",
+        )
+    )
+    write_delay("Welcome to getting started with HunG\n\n")
     write_delay("I'm going to explain everything you need to know about HunG.\n")
-    write_delay("HunG is an interpreted language that is build on top of python3,\nit is created in order to facilitate people who are getting started with programming.\nIt is also created to be as simple as possible in order to achieve this.\n\n")
-    input('Press any key to continue...')
+    write_delay(
+        "HunG is an interpreted language that is build on top of python3,\nit is created in order to facilitate people who are getting started with programming.\nIt is also created to be as simple as possible in order to achieve this.\n\n"
+    )
+    input("Press any key to continue...")
     clear_screen()
-    print(termcolor.colored('''
+    print(
+        termcolor.colored(
+            """
                                    )  (       
  (   (     )  (   (      )  ( /(  )\   (  
  )\  )\ ( /(  )(  )\  ( /(  )\())((_) ))\ 
@@ -58,17 +79,26 @@ _(())\_)() /((_)_   )\  )\    )\  ' /((_)
 \ \ / /((_)_  ((_)(_)((_)_ | |(_)| |(_))  
  \ V / / _` || '_|| |/ _` || '_ \| |/ -_) 
   \_/  \__,_||_|  |_|\__,_||_.__/|_|\___| 
-    ''',color='yellow'))
-    write_delay('Variable is the primary way that allows data to be maniputated.\n\n')
-    write_delay('In HunG, there are only 2 types of variable which is num and str.\nThe are both declared using the var keyword and the type will be auto-magically assigned from the data.\nFor example:\n')
-    print(termcolor.colored('var age = 18',color='cyan'))
-    print(termcolor.colored("var name = 'Dai Hung'",color='cyan'))
-    write_delay('In Hung programming language, the string or str data type are written in single quotation mark.\n\n')
-    input('Press any key to continue...')
+    """,
+            color="yellow",
+        )
+    )
+    write_delay("Variable is the primary way that allows data to be maniputated.\n\n")
+    write_delay(
+        "In HunG, there are only 2 types of variable which is num and str.\nThe are both declared using the var keyword and the type will be auto-magically assigned from the data.\nFor example:\n"
+    )
+    print(termcolor.colored("var age = 18", color="cyan"))
+    print(termcolor.colored("var name = 'Dai Hung'", color="cyan"))
+    write_delay(
+        "In Hung programming language, the string or str data type are written in single quotation mark.\n\n"
+    )
+    input("Press any key to continue...")
 
-    #end of getting started
+    # end of getting started
     clear_screen()
-    print(termcolor.colored('''
+    print(
+        termcolor.colored(
+            """
  (                         
  )\ )                      
 (()/(                 (    
@@ -78,57 +108,67 @@ _(())\_)() /((_)_   )\  )\    )\  ' /((_)
  | |) |/ _ \| ' \))/ -_) _ 
  |___/ \___/|_||_| \___|(_)
                            
-    ''',color='yellow'))
-    input('Press any key to return to interpreter...')
+    """,
+            color="yellow",
+        )
+    )
+    input("Press any key to return to interpreter...")
     clear_screen()
     title()
+
+
 def var_dump(datas):
-    for key,data in datas.items():
-        es(key,str(data))
+    for key, data in datas.items():
+        es(key, str(data))
     if len(datas.keys()) == 0:
-        print('No variable had been initialized')
+        print("No variable had been initialized")
+
 
 def main():
-    #main program
+    # main program
     symbol_table = interpreter.SymbolTable()
     debug = False
     running = True
     clear_screen()
     title()
     while running:
-        data = input(termcolor.colored('HunG> ',color='cyan'))
-        if data == '.clear': 
+        data = input(termcolor.colored("HunG> ", color="cyan"))
+        if data == ".clear":
             clear_screen()
             continue
-        elif data == '.exit':
+        elif data == ".exit":
             running = False
             continue
-        elif data == '.debug':
+        elif data == ".debug":
             debug = not debug
             print(f'Verbose output is {"on" if debug else "off"}!')
             continue
-        elif data == '.help':
+        elif data == ".help":
             print_help()
             continue
-        elif data == '.version':
+        elif data == ".version":
             title()
             continue
-        elif data == '.started':
+        elif data == ".started":
             getting_started()
             continue
-        elif data == '.vardump':
+        elif data == ".vardump":
             var_dump(symbol_table.dump())
             continue
-        l = lexer.Lexer(data,'<HunGConsole>').get_tokens()
+        l = lexer.Lexer(data, "<HunGConsole>").get_tokens()
         if l[0] == None:
             print(l[1])
         else:
-            if debug : print('Tokens: ',l[0])
-            p = hung_parser.Parser(l[0],'<HunGConsole>',data).parse()
+            if debug:
+                print("Tokens: ", l[0])
+            p = hung_parser.Parser(l[0], "<HunGConsole>", data).parse()
             if p[0] == None:
                 print(p[1])
             else:
-                if debug : print('AST: ',p[0])
-                i = interpreter.Interpreter(p[0],'<HunGConsole>',data,symbol_table).interpret()
+                if debug:
+                    print("AST: ", p[0])
+                i = interpreter.Interpreter(
+                    p[0], "<HunGConsole>", data, symbol_table
+                ).interpret()
                 print(i)
 
